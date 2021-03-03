3e115d389ab26da44b42999cbb93f677219968da
Raja Madasu <raja.madasu@integritymarketing.com>
2021-03-03 09:46:31 -0600

    env name updated

--- src/RandomUser.jsx
+++ src/RandomUser.jsx
@@ -1,5 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { List, ListItem, Flex, Avatar, Box, Text } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  Flex,
+  Avatar,
+  Box,
+  Text,
+  Center,
+  Spinner,
+} from "@chakra-ui/react";
 
 const RandomUser = () => {
   const [users, setUsers] = useState([]);
@@ -10,18 +19,29 @@ const RandomUser = () => {
   }, []);
   console.log({ users });
   return (
-    <Box p={20}>
-        <Text>Env App Name: {process.env.REACT_APP_APP_NAME}</Text>
-      <List spacing={3}>
-        {users.map((user) => (
-          <ListItem>
-            <Flex>
-              <Avatar size="sm" src={user.picture.thumbnail} />
-              {`${user.name.title} ${user.name.first} ${user.name.last}`}
-            </Flex>
-          </ListItem>
-        ))}
-      </List>
+    <Box bg="tomato" w="100%" h="100%" p={20}>
+      <Text fontSize="6xl" color="yellow">
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="md"
+        />
+        {process.env.REACT_APP_APP_NAME}
+      </Text>
+      <Center bg="black" h="100PX" color="white">
+        <List spacing={3}>
+          {users.map((user) => (
+            <ListItem>
+              <Flex>
+                <Avatar size="sm" src={user.picture.thumbnail} />
+                {`${user.name.title} ${user.name.first} ${user.name.last}`}
+              </Flex>
+            </ListItem>
+          ))}
+        </List>
+      </Center>
     </Box>
   );
 };
