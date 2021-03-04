80158143b932faacba3a157d6c2f4bbe6002d759
Raja Madasu <raja.madasu@integritymarketing.com>
2021-03-04 09:58:58 -0600

    Agents List page

--- src/RandomUser.jsx
+++ src/RandomUser.jsx
@@ -8,20 +8,124 @@ import {
   Text,
   Center,
   Spinner,
+  Table,
+  Tr,
+  Td,
+  Th,
+  Thead,
+  Tbody,
+  TableCaption,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
 } from "@chakra-ui/react";
+
+const ModalContent = ({ users }) => {
+  return (
+    <Center bg="black" h="300px" color="white" m={30}>
+      <List spacing={5} width={"100%"}>
+        {users.map((user) => (
+          <ListItem>
+            <Flex align={"center"} justify={"center"} direction={"column"}>
+              <h1
+                style={{
+                  color: "white",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                }}
+              >
+                Agent Information:
+              </h1>{" "}
+              <br />
+              <Avatar size="lg" src={user.picture.thumbnail} /> &nbsp; &nbsp;
+              <List spacing={3} align={"center"}>
+                <ListItem>
+                  <Text fontSize="x1" color="white" as="i">
+                    Name:{" "}
+                    {`${user.name.title} ${user.name.first} ${user.name.last}`}
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text fontSize="x1" color="white" as="i">
+                    Gender: {`${user.gender}`}
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text fontSize="x1" color="white" as="i">
+                    Phone: {`${user.phone}`}
+                  </Text>
+                </ListItem>
+                <ListItem>
+                  <Text fontSize="x1" color="white" as="i">
+                    Email: {`${user.email}`}
+                  </Text>
+                </ListItem>
+              </List>
+            </Flex>
+          </ListItem>
+        ))}
+      </List>
+    </Center>
+  );
+};
+
+const TableView = ({ users }) => (
+  <Table variant="simple">
+    <TableCaption>© 2021 Netlify Application</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>Name</Th>
+        <Th>Age</Th>
+        <Th>Email</Th>
+        <Th>Phone</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {users.map((user) => (
+        <Tr>
+          <Td>
+            <Popover>
+              <PopoverTrigger>
+                <Flex>
+                  <Box>
+                    <Avatar size="sm" src={user.picture.thumbnail} />
+                  </Box>
+                  <Box>
+                    {`${user.name.title} ${user.name.first} ${user.name.last}`}
+                  </Box>{" "}
+                </Flex>
+              </PopoverTrigger>
+              <PopoverContent>
+                <ModalContent users={[user]} />
+              </PopoverContent>
+            </Popover>
+          </Td>
+          <Td>{user.dob.age}</Td>
+          <Td>{user.email}</Td>
+          <Td>{user.phone}</Td>
+        </Tr>
+      ))}
+    </Tbody>
+  </Table>
+);
 
 const RandomUser = () => {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch("https://randomuser.me/api")
+    fetch("https://randomuser.me/api?results=20")
       .then((response) => response.json())
-      .then((data) => setUsers(data.results));
+      .then((data) => {
+        setUsers(data.results);
+        setLoading(false);
+      });
   }, []);
   return (
     <Box
-      bg="tomato"
+      bg="#4ABDAC"
       w="100%"
-      h="100vh"
+      mh="100vh"
       p={(300, 30)}
       style={{ margin: "auto" }}
     >
@@ -30,59 +134,17 @@ const RandomUser = () => {
           {process.env.REACT_APP_APP_NAME}
         </Text>
         &nbsp;
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="md"
-        />
+        {loading && (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="md"
+          />
+        )}
       </Flex>
-      <Center bg="black" h="300px" color="white" m={30}>
-        <List spacing={5} width={"100%"}>
-          {users.map((user) => (
-            <ListItem>
-              <Flex align={"center"} justify={"center"} direction={"column"}>
-                <h1
-                  style={{
-                    color: "white",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    fontSize: "20px",
-                  }}
-                >
-                  Agent Information:
-                </h1>{" "}
-                <br />
-                <Avatar size="lg" src={user.picture.thumbnail} /> &nbsp; &nbsp;
-                <List spacing={3} align={"center"}>
-                  <ListItem>
-                    <Text fontSize="x1" color="white" as="i">
-                      Name:{" "}
-                      {`${user.name.title} ${user.name.first} ${user.name.last}`}
-                    </Text>
-                  </ListItem>
-                  <ListItem>
-                    <Text fontSize="x1" color="white" as="i">
-                      Gender: {`${user.gender}`}
-                    </Text>
-                  </ListItem>
-                  <ListItem>
-                    <Text fontSize="x1" color="white" as="i">
-                      Phone: {`${user.phone}`}
-                    </Text>
-                  </ListItem>
-                  <ListItem>
-                    <Text fontSize="x1" color="white" as="i">
-                      Email: {`${user.email}`}
-                    </Text>
-                  </ListItem>
-                </List>
-              </Flex>
-            </ListItem>
-          ))}
-        </List>
-      </Center>
+      <TableView users={users} />
     </Box>
   );
 };
